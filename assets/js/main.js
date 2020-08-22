ae4d1d514a954c3a4739077f927330e0e061ddde
EitanGoren <eitangoren08@gmail.com>
2020-08-23 00:32:12 +0300

    .,m

--- assets/js/main.js
+++ assets/js/main.js
@@ -165,12 +165,35 @@
     });
   });
 
-  // Portfolio details carousel
-  $(".portfolio-details-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1
+   // Portfolio carousel isotope and filter
+   $(window).on('load', function() {
+    var portfolioIsotope = $('.testimonials-carousel').isotope({
+      itemSelector: '.testimonial-item',
+    });
+
+    $('#portfolio-flters li').on('click', function() {
+      $("#portfolio-flters li").removeClass('filter-active');
+      $(this).addClass('filter-active');
+
+      portfolioIsotope.isotope({
+        filter: $(this).data('filter')
+      });
+    });
+
+    // Initiate venobox (lightbox feature used in portofilo)
+    $(document).ready(function() {
+      $('.venobox').venobox();
+    });
   });
 
+ // Portfolio details carousel
+ $(".portfolio-details-carousel").owlCarousel({
+  autoplay: true,
+  dots: true,
+  loop: true,
+  center: true,
+  mouseDrag: true,
+  autoplayHoverPause: true,
+  items: 1
+});
 })(jQuery);
